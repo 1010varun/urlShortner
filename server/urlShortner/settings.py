2ec23eb6b6182bad52ca97c90be0e84c4d689d8a
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-08-13 16:09:00 +0530

    corrected hosts

--- server/urlShortner/settings.py
+++ server/urlShortner/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 

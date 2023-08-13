15cc7b149277e3628adc06cbb7f50d825b1165fb
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-08-13 22:02:21 +0530

    updated env variables

--- server/urlShortner/settings.py
+++ server/urlShortner/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == 'True'
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ["*"]
 

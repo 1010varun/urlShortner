de6c66922312eef26c63c8976020eaaaf8c93ae0
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-09-07 16:31:51 +0530

    updated urlCard

--- client/src/Components/urlCard.jsx
+++ client/src/Components/urlCard.jsx
@@ -12,9 +12,9 @@ const Card = ({ data, toastFunction }) => {
       className="grid bg-white border rounded-md shadow-md md:flex-row w-4/5 lg:w-4/6 xl:w-5/6 mx-auto mt-5 p-3 hover:cursor-pointer hover:shadow-cyan-800/50"
       onClick={() => copyText(data)}
     >
-      <div className="text-start my-auto overflow-x-auto flex flex-row">
+      <div className="text-start my-auto overflow-hidden flex flex-row">
           <ClipboardCopy color="#0c3fb6" className="me-3" />
-          {data}
+        { data }
       </div>
     </div>
   );
